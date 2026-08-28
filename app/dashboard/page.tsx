ffd0fb8772fac0244a5ddebcefb834f67dd7007f
nine41745-sketch <nine41745@gmail.com>
2026-08-29 03:17:00 +0700

    test: remove preview PIN test prop

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -61,7 +61,7 @@ export default async function DashboardPage() {
 
   return (
     <div className="min-h-screen bg-gray-950 p-4 md:p-8">
-      <InactivityPinLock allowPreviewFastTest={process.env.VERCEL_ENV === 'preview'} />
+      <InactivityPinLock />
       <InvestingSinceBadge />
       <PortfolioDashboard
         holdings={holdingsWithPrices}
